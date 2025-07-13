2857a0942bc497b2cb273ae4a724e03c7a90d971
fulprimitiv <eremeevdima51@gmail.com>
2025-07-14 03:50:58 +0500

    feat: верстка + стили для карточки товара

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
-window.addEventListener('DOMContentLoaded', ()=> {
+window.addEventListener('DOMContentLoaded', () => {
     console.log('App init');
 })
+
+document.addEventListener('DOMContentLoaded', function () {
+    const productCard = document.querySelector('.product-card');
+    const title = document.querySelector('.product-card__title');
+    const price = document.querySelector('.product-card__spec--price .product-card__spec-value');
+
+    productCard.addEventListener('click', function (event) {
+        console.log('УРА');
+    });
+
+    title.addEventListener('click', function (event) {
+        event.stopPropagation();
+    });
+
+    price.addEventListener('click', function (event) {
+        event.stopPropagation();
+    });
+});
