cdbdd0342ce116deed57137a6887ade5b35adce7
fulprimitiv <eremeevdima51@gmail.com>
2025-07-14 05:11:02 +0500

    feat: добавил модальное окно

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,9 +6,23 @@ document.addEventListener('DOMContentLoaded', function () {
     const productCard = document.querySelector('.product-card');
     const title = document.querySelector('.product-card__title');
     const price = document.querySelector('.product-card__spec--price .product-card__spec-value');
+    const modal = document.getElementById('successModal');
+    const modalOverlay = document.querySelector('.modal__overlay');
+    const modalCloseButton = document.querySelector('.modal__close');
+    const modalButton = document.querySelector('.modal__button');
+
+    function openModal() {
+        modal.classList.add('modal--active');
+        document.body.style.overflow = 'hidden';
+    }
+
+    function closeModal() {
+        modal.classList.remove('modal--active');
+        document.body.style.overflow = '';
+    }
 
     productCard.addEventListener('click', function (event) {
-        console.log('УРА');
+        openModal();
     });
 
     title.addEventListener('click', function (event) {
@@ -18,4 +32,10 @@ document.addEventListener('DOMContentLoaded', function () {
     price.addEventListener('click', function (event) {
         event.stopPropagation();
     });
+
+    modalOverlay.addEventListener('click', closeModal);
+
+    modalCloseButton.addEventListener('click', closeModal);
+
+    modalButton.addEventListener('click', closeModal);
 });
